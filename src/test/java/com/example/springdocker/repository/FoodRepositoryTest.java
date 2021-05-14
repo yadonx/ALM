4910c3407c1426e25332c788499bfaa8ddd060d2
yadonx <ej_yo@hotmail.com>
2021-05-14 13:35:50 +0200

    Finish test FoodRepo

--- src/test/java/com/example/springdocker/repository/FoodRepositoryTest.java
+++ src/test/java/com/example/springdocker/repository/FoodRepositoryTest.java
@@ -30,34 +30,39 @@ class FoodRepositoryTest {
     @BeforeEach
      void init(){
         repo.deleteAll();
-
-        Food f1 = new Food();
-        f1.setName("Tomato");
-        f1.setCanICookIt(true);
-        Food f2 = new Food();
-        f2.setName("Potato");
-        f2.setCanICookIt(true);
-        Food f3 = new Food();
-        f3.setName("stuff");
-        f3.setCanICookIt(false);
-
-        repo.save(f1);
-        repo.save(f2);
-        repo.save(f3);
     }
 
     @DisplayName("Find cook able food")
     @Test
     void findFoodByCanICookIt_true() {
+        Food f1 = new Food();
+        Food f2 = new Food();
+
+        f1.setName("Tomato");
+        f1.setCanICookIt(true);
+        f2.setName("Potato");
+        f2.setCanICookIt(true);
+
+        repo.save(f1);
+        repo.save(f2);
+
         List<Food> test = repo.findFoodByCanICookIt(true);
 
         assertEquals(2,test.size());
         assertEquals("Tomato", test.get(0).getName());
+        assertTrue(test.get(0).isCanICookIt());
     }
 
     @DisplayName("Find no cook able food")
     @Test
     void findFoodByCanICookIt_false() {
+        Food food = new Food();
+
+        food.setName("stuff");
+        food.setCanICookIt(false);
+
+        repo.save(food);
+
         List<Food> test = repo.findFoodByCanICookIt(false);
 
         assertEquals(1,test.size());
