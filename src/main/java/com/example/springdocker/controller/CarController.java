083d0cffd83209602beb6e36c1b63ea2281caeab
yadonx <ej_yo@hotmail.com>
2021-05-19 17:03:25 +0200

    fixed so docker works

--- src/main/java/com/example/springdocker/controller/CarController.java
+++ src/main/java/com/example/springdocker/controller/CarController.java
@@ -3,6 +3,7 @@ package com.example.springdocker.controller;
 import com.example.springdocker.model.Car;
 import com.example.springdocker.service.CarService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,7 +14,9 @@ import java.util.List;
 @RequiredArgsConstructor
 @RestController
 public class CarController {
-    private final CarService service;
+
+    @Autowired
+    private CarService service;
 
     @GetMapping("/cars")
     public List<Car> getCars() {
